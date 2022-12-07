0b4986c286ed3c470b333fdc6c18340d93875df5
Yuri <uendeligz@gmail.com>
2022-12-07 19:38:05 +0300

    Add 6 puzzle solution

---
+++ puzzles/Day6_TuningTrouble/Program.cs
@@ -0,0 +1,21 @@
+﻿using var textReader = File.OpenText("puzzleInput.txt");
+
+int numberOfChars = 14;
+
+Queue<char> sequence = new(numberOfChars);
+
+for (int i = 0; i < numberOfChars; i++)
+{
+    sequence.Enqueue((char)textReader.Read());
+}
+
+int currentChar = numberOfChars;
+
+while (sequence.Distinct().Count() < numberOfChars && !textReader.EndOfStream)
+{
+    sequence.Enqueue((char)textReader.Read());
+    sequence.Dequeue();
+    currentChar++;
+}
+
+Console.WriteLine(currentChar);
